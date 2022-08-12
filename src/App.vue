b4c317f80cfebcc194250b43994ec7131507d0b1
Gustavo Freitas <gustavo.freitas@contractor.outroll.com>
2022-08-12 12:55:06 -0300

    inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BoardRoot />
+  <h1 @click="emitSocket">Testing message</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BoardRoot from './components/BoardRoot.vue'
+import SocketioService from './services/socketio.service.js'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BoardRoot
+  },
+  data () {
+    return {
+      socket: null
+    }
+  },
+  created() {
+    this.socket = SocketioService
+    this.socket.setupSocketConnection()
+  },
+  methods: {
+    emitSocket () {
+      this.socket.socket.emit('play', { payload: 123 })
+    }
   }
 }
 </script>
---
+++ src/components/BoardRoot.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="board">
+    <div class="row" v-for="row in coordinates" :key="row">
+      <div :id="`${row}${col}`" class="square" v-for="col of 8" :key="`${row}${col}`"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    coordinates: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+  })
+}
+</script>
+
+<style>
+.board {
+  display: flex;
+  width: min-content;
+  height: auto;
+  flex-direction: column;
+  margin: 0 auto;
+  border: 3px solid #222;
+  box-shadow: 1px 2px 10px rgba(55, 55, 55, .5);
+}
+
+.row {
+  display: grid;
+  grid-template-columns: repeat(8, 50px);
+}
+
+.square{
+  height: 50px;
+}
+
+.board>div:nth-child(odd)>div:nth-child(even),
+.board>div:nth-child(even)>div:nth-child(odd){
+  background-color:black; color:white;
+}
+
+.board>div:nth-child(1), .board>div>div:nth-child(1) {
+  background-color: white;
+}
+</style>
